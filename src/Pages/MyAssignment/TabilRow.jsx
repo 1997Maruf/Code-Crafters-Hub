65acd1bd6c30602508d1e3d0169e898844b04470
maruf hossen <mdmdrufmeze@gmail.com>
2024-05-15 06:52:24 -0700

    add style code

--- src/Pages/MyAssignment/TabilRow.jsx
+++ src/Pages/MyAssignment/TabilRow.jsx
@@ -4,10 +4,10 @@ const TabilRow = ({submit}) => {
     const {assignmentTitle,marks} = submit;
 
     return (
-        <div className="ml-20">
+        <div className="ml-20 w-1/2 border-2">
             <tr>
         
-        <td className="border-2">{assignmentTitle}</td>
+        <td >{assignmentTitle}</td>
         <td>{marks}</td>
         
       </tr>
