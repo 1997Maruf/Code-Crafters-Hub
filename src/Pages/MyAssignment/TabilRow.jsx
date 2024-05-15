a06ba6f93a1c83ed17f1d50122cc8dac31e40be4
maruf hossen <mdmdrufmeze@gmail.com>
2024-05-15 07:57:58 -0700

    add

--- src/Pages/MyAssignment/TabilRow.jsx
+++ src/Pages/MyAssignment/TabilRow.jsx
@@ -4,7 +4,7 @@ const TabilRow = ({submit}) => {
     const {assignmentTitle,marks} = submit;
 
     return (
-        <div className="ml-20 w-1/2 shadow-2xl shadow-slate-700 border-2">
+        <div className="ml-20 w-1/2 shadow-md shadow-slate-700 border-2">
             <tr>
         
         <td  className="mt-4">{assignmentTitle}</td>
